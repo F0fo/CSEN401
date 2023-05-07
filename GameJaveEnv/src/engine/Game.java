19fc6d6f1da30bee1b326b9fcf8446492f6427f3
Farah065 <farahahmad065@gmail.com>
2023-05-07 23:35:47 +0300

    M2 Base

--- GameJaveEnv/src/engine/Game.java
+++ GameJaveEnv/src/engine/Game.java
@@ -2,12 +2,22 @@ package engine;
 import java.util.ArrayList;
 
 import model.characters.*;
+import model.collectibles.Supply;
+import model.collectibles.Vaccine;
 import model.world.Cell;
+import model.world.CharacterCell;
+import model.world.CollectibleCell;
+import model.world.TrapCell;
 
+import java.awt.Point;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
+import exceptions.InvalidTargetException;
+import exceptions.MovementException;
+import exceptions.NotEnoughActionsException;
    
 public class Game {
     public static ArrayList<Hero> availableHeroes;
@@ -42,14 +52,174 @@ public class Game {
 
         br.close();
     }
-
-    public static void main(String[] args) {
-        map = new Cell[15][15];
-        zombies = new ArrayList<Zombie>();
-        heroes = new ArrayList<Hero>();
-
-        for(int i = 0; i < 10; i++) {
-            zombies.add(new Zombie());
-        }
+    
+    public static void startGame(Hero h) {
+    	map = new Cell[15][15];
+    	map[14][0] = new CharacterCell(h);
+    	// safety check?
+    	
+    	availableHeroes.remove(h);
+    	heroes = new ArrayList<Hero>();
+    	heroes.add(h);
+    	h.setLocation(new Point(0, 0));
+    	
+    	zombies = new ArrayList<Zombie>();
+    	
+    	for(int i = 0; i < 25; i++) {
+    		int r = (int)(Math.random() * 15);
+    		int c = (int)(Math.random() * 15);
+    		while(map[r][c] != null) {
+    			r = (int)(Math.random() * 15);
+        		c = (int)(Math.random() * 15);
+    		}
+    		if(i < 5) {
+    			Vaccine v = new Vaccine();
+    			map[r][c] = new CollectibleCell(v);
+    		}
+    		else if(i < 10) {
+    			Supply s = new Supply();
+        		map[r][c] = new CollectibleCell(s);
+    		}
+    		else if(i < 15) {
+    			map[r][c] = new TrapCell();
+    		}
+    		else {
+    			Zombie z = new Zombie();
+    			z.setLocation(new Point(14 - r, c));
+    			map[r][c] = new CharacterCell(z);
+    			((CharacterCell)map[r][c]).setSafe(false);
+    			zombies.add(z);
+    		}
+    	}
+    	
+    	for(int i = 0; i < 15; i++) {
+    		for(int j = 0; j < 15; j++) {
+    			if(map[i][j] == null)
+    				map[i][j] = new CharacterCell(null);
+    		}
+    	}
+    	
+    	updateVisibility();
     }
+    
+    public static boolean checkWin() {
+    	for(int i = 0; i < 15; i++) {
+    		for(int j = 0; j < 15; j++) {
+    			if(map[i][j] instanceof CollectibleCell) {
+    				if(((CollectibleCell)map[i][j]).getCollectible() instanceof Vaccine)
+    					return false;
+    			}
+    		}
+    	}
+    	if(heroes.size() < 5)
+    		return false;
+    	for(int i = 0; i < heroes.size(); i++) {
+    		if(!(heroes.get(i).getVaccineInventory().isEmpty()))
+    			return false;
+    	}
+    	return true;
+    }
+    
+    public static boolean checkGameOver() {
+    	return heroes.isEmpty();
+    }
+    
+    public static void endTurn() throws NotEnoughActionsException, InvalidTargetException {
+    	for(int i = 0; i < 15; i++) {
+    		for(int j = 0; j < 15; j++) {
+    			map[i][i].setVisible(false);
+    			if(map[i][j] instanceof CharacterCell) {
+    				if(((CharacterCell)map[i][j]).getCharacter() instanceof Hero) {
+    					Hero h = (Hero)((CharacterCell)map[i][j]).getCharacter();
+    					h.setTarget(null);
+    					h.setActionsAvailable(h.getMaxActions());
+    					h.setSpecialAction(false);
+    				}
+    				if(((CharacterCell)map[i][j]).getCharacter() instanceof Zombie) {
+    					if(i > 0 && ((CharacterCell)map[i - 1][j]).getCharacter() instanceof Hero){
+    						((CharacterCell)map[i][j]).getCharacter().setTarget(((CharacterCell)map[i - 1][j]).getCharacter());
+    						((CharacterCell)map[i][j]).getCharacter().attack();
+    					}
+    					else if(i < 14 && ((CharacterCell)map[i + 1][j]).getCharacter() instanceof Hero) {
+    						((CharacterCell)map[i][j]).getCharacter().setTarget(((CharacterCell)map[i + 1][j]).getCharacter());
+    						((CharacterCell)map[i][j]).getCharacter().attack();
+    					}
+    					else if(j > 0 && ((CharacterCell)map[i][j - 1]).getCharacter() instanceof Hero) {
+    						((CharacterCell)map[i][j]).getCharacter().setTarget(((CharacterCell)map[i][j - 1]).getCharacter());
+    						((CharacterCell)map[i][j]).getCharacter().attack();
+    					}
+    					else if(j < 14 && ((CharacterCell)map[i][j + 1]).getCharacter() instanceof Hero) {
+    						((CharacterCell)map[i][j]).getCharacter().setTarget(((CharacterCell)map[i][j + 1]).getCharacter());
+    						((CharacterCell)map[i][j]).getCharacter().attack();
+    					}
+    				}
+    			}
+    		}
+    	}
+    	
+    	int r = (int)(Math.random() * 15);
+    	int c = (int)(Math.random() * 15);
+    	while(!(Game.map[r][c] instanceof CharacterCell && ((CharacterCell)Game.map[r][c]).getCharacter() == null)) {
+    		r = (int)(Math.random() * 15);
+        	c = (int)(Math.random() * 15);
+    	}
+    	Zombie z = new Zombie();
+    	z.setLocation(new Point(14 - r, c));
+    	Game.map[r][c] = new CharacterCell(z);
+    	// check safety?
+    	Game.zombies.add(z);
+    }
+    
+    public static void updateVisibility() {
+    	for(int i = 0; i < 15; i++) {
+    		for(int j = 0; j < 15; j++) {
+    			if(map[i][j] instanceof CharacterCell && ((CharacterCell)map[i][j]).getCharacter() instanceof Hero) {
+					map[i][j].setVisible(true);
+					if(i > 0)
+						map[i - 1][j].setVisible(true);
+					if(i < 14)
+						map[i + 1][j].setVisible(true);
+					if(j > 0)
+						map[i][j - 1].setVisible(true);
+					if(j < 14)
+						map[i][j + 1].setVisible(true);
+    			}
+    		}
+    	}
+    }
+    
+   /*public static void main(String[] args) throws IOException, FileNotFoundException, MovementException {
+    	availableHeroes = new ArrayList<Hero>();
+		Game.loadHeroes("D:\\University\\Semester 4\\Computer Programming Lab\\Game\\Milestone 1\\Heroes CSV File\\Heros.csv"); // path. hmm
+		
+    	startGame(availableHeroes.get(0));
+    	printMap();
+    }
+    
+    public static void printMap(){
+    	for(int i = 0; i < map.length; i++){
+    		for(int j = 0; j < map[i].length; j++){
+    			if(map[i][j] == null)
+    				System.out.print("[ ]");
+    			else{
+    				if(map[i][j] instanceof CharacterCell){
+    					if(((CharacterCell)map[i][j]).getCharacter() instanceof Hero)
+    						System.out.print("[H]");
+    					else if(((CharacterCell)map[i][j]).getCharacter() instanceof Zombie)
+    						System.out.print("[Z]");
+    				}
+    				else if(map[i][j] instanceof CollectibleCell){
+    					if(((CollectibleCell) map[i][j]).getCollectible() instanceof Vaccine)
+    						System.out.print("[V]");
+    					else if(((CollectibleCell) map[i][j]).getCollectible() instanceof Supply)
+    						System.out.print("[S]");
+    				}
+    				else if(map[i][j] instanceof TrapCell){
+    					System.out.print("[T]");
+    				}
+    			}
+    		}
+    		System.out.println();
+    	}
+    }*/
 }
--- GameJaveEnv/src/model/characters/Zombie.java
+++ GameJaveEnv/src/model/characters/Zombie.java
@@ -1,5 +1,10 @@
 package model.characters;
 
+import java.awt.Point;
+
+import model.world.CharacterCell;
+import engine.Game;
+
 public class Zombie extends Character {
     private static int ZOMBIES_COUNT = 0;
     private final static int MAXHP = 40;
@@ -7,5 +12,20 @@ public class Zombie extends Character {
     
     public Zombie() {
         super("Zombie " + (++ZOMBIES_COUNT), MAXHP, DAMAGE);
+    }
+    
+    public void onCharacterDeath() {
+    	super.onCharacterDeath();
+    	int r = (int)(Math.random() * 15);
+    	int c = (int)(Math.random() * 15);
+    	while(!(Game.map[r][c] instanceof CharacterCell && ((CharacterCell)Game.map[r][c]).getCharacter() == null)) {
+    		r = (int)(Math.random() * 15);
+        	c = (int)(Math.random() * 15);
+    	}
+    	Zombie z = new Zombie();
+    	z.setLocation(new Point(14 - r, c));
+    	Game.map[r][c] = new CharacterCell(z);
+    	// check safety?
+    	Game.zombies.add(z);
     }
 }
